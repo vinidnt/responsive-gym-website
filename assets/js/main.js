e33a302e950608d02aa5351241c59c46ffd569c6
vinidnt <vinicius.wasp@gmail.com>
2022-08-15 21:50:06 -0300

    HTML, CSS e JS da seção de calcular o IMC

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,6 @@ const linkAction = () => {
 	const navMenu = document.getElementById('nav-menu')
 	//When we click on each nav__link, we remove the show-menu class
 	navMenu.classList.remove('show-menu')
-	console.log("entrou 2")
 }
 
 navLink.forEach(e => e.addEventListener('click', linkAction))
@@ -36,7 +35,6 @@ navLink.forEach(e => e.addEventListener('click', linkAction))
 
 const scrollHeader = () => {
 	const header = document.getElementById('header')
-	console.log("entrou 1")
 	//When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
 	//
 	this.scrollY >= 50 ? header.classList.add('bg-header')
@@ -54,6 +52,60 @@ const scrollHeader = () => {
 
 
 /*=============== CALCULATE JS ===============*/
+const calculateForm = document.getElementById('calculate-form'),
+      calculateCm = document.getElementById('calculate-cm'),
+      calculateKg = document.getElementById('calculate-kg'),
+      calculateMessage = document.getElementById('calculate-message');
+
+const calculateBmi = (e) => {
+	e.preventDefault()
+
+	//Checks if the fields have a value
+	if(calculateCm.value === '' || calculateKg.value === ''){
+		//Add and remove color
+		calculateMessage.classList.remove('color-green')
+		calculateMessage.classList.add('color-red')
+
+		//Show message
+		calculateMessage.textContent = 'Fill in the Height and Weight 👨‍💻'
+
+		setTimeout(() => {
+			calculateMessage.textContent = ''
+		}, 5000)
+	} else {
+		//BMi Formula
+		const cm = calculateCm.value /100,
+		      kg = calculateKg.value,
+		      bmi = Math.round(kg / (cm * cm))
+		
+		//Show yout health status
+		if(bmi < 18.5){
+			//Add color and display message
+			calculateMessage.classList.add('color-green')
+			calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny 😔`
+		}  else if(bmi < 25){
+			calculateMessage.classList.add('color-green')
+			calculateMessage.textContent = `Your BMI is ${bmi} and you are healthy 🥳`
+		} else {
+			calculateMessage.classList.add('color-green')
+			calculateMessage.textContent = `Your BMI is ${bmi} and you are overwight 😔`
+		}
+
+		// To clear the input field
+		calculateCm.value = ''
+		calculateKg.value = ''
+
+		//Remove message
+		setTimeout (() => {
+			calculateMessage.value = ''
+		}, 5000)
+	}
+
+}
+
+calculateForm.addEventListener('submit', calculateBmi)
+
+
 
 
 /*=============== EMAIL JS ===============*/
